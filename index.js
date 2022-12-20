21f3f7016d03ea496e8693eeb0801bf2feea3681
gabrielyap <glyap@ucdavis.edu>
2022-12-19 18:23:23 -0800

    displayed time and started break buttons

--- index.js
+++ index.js
@@ -1,5 +1,53 @@
 function App(){
-    return <div>"hello world"</div>;
+    const [displayTime, setDisplayTime] = React.useState(50*60);
+    const timeToMins = (time) =>{
+        let mins = Math.floor(time / 60);
+        let secs = time % 60;
+        let newTime = "";
+        if(mins < 10){
+            newTime += ("0" + mins + ":");
+            console.log(newTime);
+        }
+        else{
+            newTime += mins + ":";
+            console.log(newTime);
+        }
+        if(secs < 10){
+            newTime += ("0" + secs);
+            console.log(newTime);
+        }
+        else{
+            newTime += secs;
+            console.log(newTime);
+        }
+        return newTime
+    };
+    return(
+        <div>
+            <Length
+            title = {"Break Length"} 
+            changeTime = {null} 
+            type = {"break"} 
+            time = {null} 
+            formatTime = {timeToMins}
+            />
+            <h1>{timeToMins(displayTime)}</h1> 
+
+        </div>
+    );
 }
 
-ReactDOM.render(<App />, document.getElementById("root"));
+function Length({title, changeTime, type, time, formatTime}){
+    return(
+        <div>
+            <h3>{title}</h3>
+            <div className = "time-sets">
+                <button className = "btn-small blue">
+                    <i className="material-icons">arrow_downward</i>
+                </button>
+            </div>
+        </div>
+    );
+}
+const root = ReactDOM.createRoot(document.getElementById('root')); //need REACTDOM before createRoot
+root.render(<App />);
